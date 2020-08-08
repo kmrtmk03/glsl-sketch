f7e21bf59536d83100b6ed0e85ccccbc9c58f92c
Kimura Tomoki <krmmmmmmrk@gmail.com>
2020-08-09 03:08:06 +0900

    update sketch_4

--- sketch_4/shader.glsl
+++ sketch_4/shader.glsl
@@ -5,24 +5,13 @@ uniform vec2 resolution;
 void main( void ) {
 
 	vec2 p = 1.0 * (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
+	
+	float len = 0.05 / length(p.x + sin(exp(p.y + cos(p.x) * p.x) + time));
 
-
-	float len = 0.01 / length(p.x + sin(p.y * 5.0 + time));
-
-
-	float len2 = 0.5 / length(p.x + sin(p.y * 2.0) + 0.2);
-
-	len *= len2;
-
-	float circle = 2.5 / length(vec2(sin(p.x + time), p.y));
-
-	len *= circle;
-
-
-	vec3 color = 1.0 - vec3(step(len, 0.3));
-
-
-
-	gl_FragColor = vec4(color, 1.0);
+	float d = dot(p, vec2(len));
+	
+	vec3 dest = vec3(step(d - p.x, 0.5), step(d * p.y + p.x, cos(p.x * p.y * time / 200.0)), step(d / p.y, 0.5));
+	
+	gl_FragColor = vec4(dest, 1.0);
 
 }
